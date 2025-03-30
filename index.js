eb02bb33d7654b1d513607c080921cdb97a536be
Aaron Bandado <aaron.aminu1@gmail.com>
2025-03-30 14:41:03 +0300

    project  blue print

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+
+// init project
+var express = require('express');
+var app = express();
+
+
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  
+
+
+app.use(express.static('public'));
+
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
